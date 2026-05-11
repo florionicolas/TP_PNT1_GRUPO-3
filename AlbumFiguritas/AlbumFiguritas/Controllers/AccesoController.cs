459a1f614f0c59417f123b5e933eba384a9c116f
Gabriel Mendoza <gabilean95@gmail.com>
2026-05-11 19:03:07 -0300

    Se añadió interfaz de login, falta corregir estilo, remover navbar y cambiar el footer

---
+++ AlbumFiguritas/AlbumFiguritas/Controllers/AccesoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AlbumFiguritas.Controllers
+{
+    public class AccesoController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
