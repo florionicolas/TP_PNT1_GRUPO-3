be7d30465fa8fb72dc8aa5e2b66f8fc63d7fa230
Gabriel Mendoza <gabilean95@gmail.com>
2026-05-11 18:53:05 -0300

    testPush creacion de proyecto con MVC

---
+++ AlbumFiguritas/AlbumFiguritas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AlbumFiguritas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
