f6ac308737942530062542df8cc6dfd8567dfa0a
Gabriel Mendoza <gabilean95@gmail.com>
2026-05-19 00:44:23 -0300

    Agregué vista de registro, asociada a boton "Registrar" del Login

--- AlbumFiguritas/AlbumFiguritas/Controllers/AccesoController.cs
+++ AlbumFiguritas/AlbumFiguritas/Controllers/AccesoController.cs
@@ -4,9 +4,31 @@ namespace AlbumFiguritas.Controllers
 {
     public class AccesoController : Controller
     {
+        [HttpGet]
         public IActionResult Login()
         {
             return View();
         }
+
+        [HttpGet]
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Registro(string nombreUsuario, string email, string password, string confirmarPassword)
+        {
+            if (password != confirmarPassword)
+            {
+                ModelState.AddModelError(string.Empty, "Las contraseñas no coinciden.");
+                return View();
+            }
+            // Aquí iría la lógica para registrar al usuario en la base de datos
+            // Por ejemplo: _userService.Register(username, password);
+            return RedirectToAction("Registro");
+        }
     }
+
+
 }
