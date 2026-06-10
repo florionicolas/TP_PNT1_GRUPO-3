d362c7d84509b5bf50f369b442a791f7976af20c
borgnae <ezborgna@gmail.com>
2026-06-09 23:58:09 -0300

    Gestionar Álbum: creación de la vista, conexión de AlbumController a Entity Framework y visualización dinámica de figuritas.-

--- AlbumFiguritas/AlbumFiguritas/Controllers/AlbumController.cs
+++ AlbumFiguritas/AlbumFiguritas/Controllers/AlbumController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AlbumFiguritas.Context;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AlbumFiguritas.Controllers
 {
     public class AlbumController : Controller
     {
+        private readonly AlbumDatabaseContext _context;
+
+        public AlbumController(AlbumDatabaseContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Gestion()
         {
-            return View();
+            var figuritas = _context.Figuritas.ToList();
+            return View(figuritas);
         }
 
         public IActionResult MiAlbum()
