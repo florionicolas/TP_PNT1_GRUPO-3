34117ad1ccd9e829f91039fb9e8997eec6b8f8fc
florionicolas <florio.nicolas13@gmail.com>
2026-06-22 19:55:21 -0300

    se modifica db con errores typo

---
+++ AlbumFiguritas/AlbumFiguritas/Migrations/20260622225359_ArreglarTypo.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AlbumFiguritas.Migrations
+{
+    /// <inheritdoc />
+    public partial class ArreglarTypo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Figuritas",
+                keyColumn: "Id",
+                keyValue: 26,
+                column: "NombreJugador",
+                value: "Kingsley Koman");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Figuritas",
+                keyColumn: "Id",
+                keyValue: 26,
+                column: "NombreJugador",
+                value: "Kingsley Coman");
+        }
+    }
+}
